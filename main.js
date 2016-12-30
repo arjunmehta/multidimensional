dd01e0167571e738ae08e936feef7402b2f371eb
Arjun Mehta <arjunmeht@gmail.com>
2016-12-30 10:36:06 -0700

    Code cleanup

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 function MultiDimensional(dimensions, cb) {
+  var initialPosition = createInitialArray([], dimensions.length);
 
-    var initialPosition = createInitialArray([], dimensions.length);
+  this.dimensions = dimensions;
+  this.matrix = createNDimensionArray(this, dimensions, initialPosition, cb);
 
-    this.dimensions = dimensions;
-    this.matrix = createNDimensionArray(this, dimensions, initialPosition, cb);
-
-    return this;
+  return this;
 }
 
 MultiDimensional.prototype.position = function(position, value) {
+  var returnValue;
 
-    var returnValue;
+  if (value === undefined) {
+    returnValue = getElement(this.matrix, position);
+  } else {
+    returnValue = setElement(this.matrix, position, value);
+  }
 
-    if (value === undefined) {
-        returnValue = getElement(this.matrix, position);
-    } else {
-        returnValue = setElement(this.matrix, position, value);
-    }
-
-    return returnValue;
+  return returnValue;
 };
 
 
@@ -26,78 +24,71 @@ MultiDimensional.prototype.position = function(position, value) {
 // from: http://stackoverflow.com/questions/12588618/javascript-n-dimensional-array-creation/12588826#12588826
 
 function createNDimensionArray(multidimensional, dimensions, position, cb) {
+  var width;
+  var rest;
+  var newArray;
+  var i;
+  var currentDimension;
+  var returnValue;
 
-    var width;
-    var rest;
-    var newArray;
-    var i;
-    var currentDimension;
-    var returnValue;
+  if (dimensions.length > 0) {
+    width = dimensions[0];
+    rest = dimensions.slice(1);
+    newArray = [];
+    currentDimension = position.length - 1 - rest.length;
+    position[currentDimension] = 0;
 
-    if (dimensions.length > 0) {
-
-        width = dimensions[0];
-        rest = dimensions.slice(1);
-        newArray = [];
-        currentDimension = position.length - 1 - rest.length;
-        position[currentDimension] = 0;
-
-        for (i = 0; i < width; i++) {
-            newArray[i] = createNDimensionArray(multidimensional, rest, position, cb);
-            position[currentDimension]++;
-        }
-
-        returnValue = newArray;
-
-    } else {
-
-        if (typeof cb === 'function') {
-            returnValue = cb(position.slice(), multidimensional);
-        } else if (cb !== undefined) {
-            returnValue = cb;
-        } else {
-            returnValue = null;
-        }
+    for (i = 0; i < width; i++) {
+      newArray[i] = createNDimensionArray(multidimensional, rest, position, cb);
+      position[currentDimension]++;
     }
 
-    return returnValue;
+    returnValue = newArray;
+  } else {
+    if (typeof cb === 'function') {
+      returnValue = cb(position.slice(), multidimensional);
+    } else if (cb !== undefined) {
+      returnValue = cb;
+    } else {
+      returnValue = null;
+    }
+  }
+
+  return returnValue;
 }
 
 function getElement(array, indices) {
+  var returnValue;
 
-    var returnValue;
+  if (indices.length === 0) {
+    returnValue = array;
+  } else {
+    returnValue = getElement(array[indices[0]], indices.slice(1));
+  }
 
-    if (indices.length === 0) {
-        returnValue = array;
-    } else {
-        returnValue = getElement(array[indices[0]], indices.slice(1));
-    }
-
-    return returnValue;
+  return returnValue;
 }
 
 function setElement(array, indices, value) {
+  var returnValue;
 
-    var returnValue;
+  if (indices.length === 1) {
+    array[indices[0]] = value;
+    returnValue = value;
+  } else {
+    returnValue = setElement(array[indices[0]], indices.slice(1), value);
+  }
 
-    if (indices.length === 1) {
-        array[indices[0]] = value;
-        returnValue = value;
-    } else {
-        returnValue = setElement(array[indices[0]], indices.slice(1), value);
-    }
-
-    return returnValue;
+  return returnValue;
 }
 
 function createInitialArray(arr, length) {
+  var i;
+  for (i = 0; i < length; i++) {
+    arr[i] = 0;
+  }
 
-    var i;
-    for (i = 0; i < length; i++) {
-        arr[i] = 0;
-    }
-
-    return arr;
+  return arr;
 }
 
 
--- tests/test.js
+++ tests/test.js
@@ -5,53 +5,47 @@ var test = require('tape').test;
 // tests
 
 test('Methods Exist', function(t) {
+  var multidimensional = new MultiDimensional([3, 4, 5]);
 
-    var multidimensional = new MultiDimensional([3, 4, 5]);
-
-    t.equal(multidimensional.position([0, 0, 0]), null);
-    t.equal(Array.isArray(multidimensional.matrix), true);
-    t.end();
+  t.equal(multidimensional.position([0, 0, 0]), null);
+  t.equal(Array.isArray(multidimensional.matrix), true);
+  t.end();
 });
 
 
 test('Set all to a String', function(t) {
+  var multidimensional = new MultiDimensional([5, 4, 3], 'Hi there');
 
-    var multidimensional = new MultiDimensional([5, 4, 3], 'Hi there');
-
-    t.equal(multidimensional.position([3, 2, 1]), 'Hi there');
-    t.end();
+  t.equal(multidimensional.position([3, 2, 1]), 'Hi there');
+  t.end();
 });
 
 test('Set a Value to a String', function(t) {
+  var multidimensional = new MultiDimensional([5, 4, 3]);
 
-    var multidimensional = new MultiDimensional([5, 4, 3]);
+  t.equal(multidimensional.position([3, 2, 1]), null);
+  multidimensional.position([3, 2, 1], 'Jambalaya');
+  t.equal(multidimensional.position([3, 2, 1]), 'Jambalaya');
 
-    t.equal(multidimensional.position([3, 2, 1]), null);
-    multidimensional.position([3, 2, 1], 'Jambalaya');
-    t.equal(multidimensional.position([3, 2, 1]), 'Jambalaya');
-
-    t.end();
+  t.end();
 });
 
 
 test('Set a Values to an instance of an Object', function(t) {
+  var threedimensional = new MultiDimensional([3, 3, 4], function(position, multidimensional) {
+    return new Cell(position, multidimensional.dimensions);
+  });
 
-    var threedimensional = new MultiDimensional([3, 3, 4], function(position, multidimensional) {
-        return new Cell(position, multidimensional.dimensions);
-    });
+  // console.log(JSON.stringify(threedimensional));
+  t.equal(threedimensional.position([2, 2, 1]).position[0], 2);
+  t.equal(threedimensional.position([2, 2, 1]).position[1], 2);
+  t.equal(threedimensional.position([2, 2, 1]).position[2], 1);
 
-    // console.log(JSON.stringify(threedimensional));
-
-    t.equal(threedimensional.position([2, 2, 1]).position[0], 2);
-    t.equal(threedimensional.position([2, 2, 1]).position[1], 2);
-    t.equal(threedimensional.position([2, 2, 1]).position[2], 1);
-
-    t.end();
-
+  t.end();
 });
 
 
 function Cell(position, overallDimensions) {
-    this.position = position;
-    this.overallDimensions = overallDimensions;
+  this.position = position;
+  this.overallDimensions = overallDimensions;
 }
